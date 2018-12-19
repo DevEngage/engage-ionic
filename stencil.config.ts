bdaed73e36aba2c6f74d2350a10132b0bc5c081a
James Brinkerhoff <th3brink@gmail.com>
2018-12-19 00:01:42 -0700

    added sass support

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,11 @@
 import { Config } from '@stencil/core';
-
+import { sass } from '@stencil/sass';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  plugins: [
+    sass()
+  ],
   outputTargets: [
     {
       type: 'www'

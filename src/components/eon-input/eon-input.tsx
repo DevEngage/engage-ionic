bdd624f7d2e4f530d22ed9d91ed491c5ea3a3fca
James Brinkerhoff <th3brink@gmail.com>
2018-12-18 23:57:26 -0700

    initial eon-input

---
+++ src/components/eon-input/eon-input.tsx
@@ -0,0 +1,18 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+    tag: 'eon-input',
+    styleUrl: 'eon-input.css'
+})
+export class EonInput {
+
+    
+
+    render() {
+        return (
+            <ion-input>
+            </ion-input>
+        );
+    }
+}

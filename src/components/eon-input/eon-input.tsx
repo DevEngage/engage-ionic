cbe1fcceaa7a8dc29a5d4de1d6ad0cd4fb14875d
James Brinkerhoff <th3brink@gmail.com>
2018-12-26 22:53:31 -0700

    added prettier to the project

--- src/components/eon-input/eon-input.tsx
+++ src/components/eon-input/eon-input.tsx
@@ -1,28 +1,27 @@
-import {Component, Prop} from '@stencil/core';
-
+import { Component, Prop } from '@stencil/core';
 
 @Component({
-    tag: 'eon-input',
-    styleUrl: 'eon-input.scss'
+  tag: 'eon-input',
+  styleUrl: 'eon-input.scss'
 })
 export class EonInput {
+  @Prop() accept: string | undefined;
+  @Prop() autocapitalize: string;
+  @Prop() autocomplete: 'off' | 'on';
+  @Prop() autocorrect: 'off' | 'on';
+  @Prop() autofocus: boolean;
+  @Prop() bindSelector = '[eon-bind]';
+  @Prop() errorSelector = '[eon-error]';
 
-    @Prop() accept: string | undefined;
-    @Prop() autocapitalize: string;
-    @Prop() autocomplete: 'off' | 'on';
-    @Prop() autocorrect: 'off' | 'on';
-    @Prop() autofocus: boolean;
-
-    render() {
-        return (
-            <ion-input
-              accept={this.accept}
-              autocapitalize={this.autocapitalize}
-              autocomplete={this.autocomplete}
-              autocorrect={this.autocorrect}
-              autofocus={this.autofocus}
-            >
-            </ion-input>
-        );
-    }
+  render() {
+    return (
+      <ion-input
+        accept={this.accept}
+        autocapitalize={this.autocapitalize}
+        autocomplete={this.autocomplete}
+        autocorrect={this.autocorrect}
+        autofocus={this.autofocus}
+      />
+    );
+  }
 }

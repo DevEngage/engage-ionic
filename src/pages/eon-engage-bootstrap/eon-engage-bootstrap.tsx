b10ae7e18a6f201a3f068cdd664347cc7eda975c
James Brinkerhoff <th3brink@gmail.com>
2019-01-11 20:50:50 -0700

    prepped the repo for npm

--- src/pages/eon-engage-bootstrap/eon-engage-bootstrap.tsx
+++ src/pages/eon-engage-bootstrap/eon-engage-bootstrap.tsx
@@ -27,6 +27,7 @@ export class EonEngageBootstrap {
           <h1 class="m-1">margin 1</h1>
           <h1 class="m-2">margin 2</h1>
           <h1 class="m-5">margin 5</h1>
+          <h1 class="m-10">margin 10</h1>
         </div>
 
       </ion-content>
--- stencil.config.ts
+++ stencil.config.ts
@@ -3,14 +3,14 @@ import { sass } from '@stencil/sass';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  namespace: 'eon',
   plugins: [
     sass()
   ],
   outputTargets: [
-    {
-      type: 'www'
-    }
+    { type: 'www' },
+    { type: 'dist' }
   ],
-  globalScript: 'src/global/app.ts',
-  globalStyle: 'src/global/app.css'
+  globalScript: 'src/global/eon.ts',
+  globalStyle: 'src/global/eon.css'
 };

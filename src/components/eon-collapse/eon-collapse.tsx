0e8f3a762421f66a376c6dbaa47b47cc9bd32964
James Brinkerhoff <th3brink@gmail.com>
2019-01-19 13:32:36 -0700

    start of collapse

---
+++ src/components/eon-collapse/eon-collapse.tsx
@@ -0,0 +1,19 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+    tag: 'eon-collapse',
+    styleUrl: 'eon-collapse.scss'
+})
+export class EonCollapse {
+
+    
+
+    render() {
+        return (
+            <div>
+                <p>Hello EonCollapse!</p>
+            </div>
+        );
+    }
+}
